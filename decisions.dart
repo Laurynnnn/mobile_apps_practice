4e2099eaa0c472033a885092dfb9d0838642fa34
Lauryn Laeticia Kantono <87691653+Laurynnnn@users.noreply.github.com>
2022-11-11 03:57:57 -0800

    Add files via upload
    Signed-off-by: Lauryn Laeticia Kantono <87691653+Laurynnnn@users.noreply.github.com>

---
+++ decisions.dart
@@ -0,0 +1,16 @@
+int main()
+{
+    int x = 5;
+    int y = 1;
+    if (x>y){
+        print("x is greater than y");
+    }
+    else if(x<y){
+        print("x is less than y");
+    }
+    else{
+        print("x is equal to y");
+    };
+
+    return 0;
+}
