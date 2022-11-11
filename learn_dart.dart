8e1430ce5b96276f203e8df6414780c3bedc9117
Lauryn Laeticia Kantono <87691653+Laurynnnn@users.noreply.github.com>
2022-11-11 03:39:08 -0800

    Add files via upload
    Signed-off-by: Lauryn Laeticia Kantono <87691653+Laurynnnn@users.noreply.github.com>

---
+++ learn_dart.dart
@@ -0,0 +1,37 @@
+void main()
+{
+    // strings
+    print("Hello Lauryn!");
+
+    var name = "Lauryn";
+    var age = 22;
+
+    print("My name is ${name} and I am ${age} years old.");
+
+    // operators
+    int a = 2;
+    int c = 6;
+
+    int d = a*c;
+
+    print("$a * $c = $d");
+
+    // loops
+    print("\n");
+    print("from for loop");
+    for(int i = 10; i > 0; i--)
+    {
+        print("Laurynnnn");
+    }
+
+    print("\n");
+    print("from while loop");
+    int i = 0;
+    while(i<10)
+    {
+        print("Laurynnnn");
+        i++;
+    }
+
+}
+
